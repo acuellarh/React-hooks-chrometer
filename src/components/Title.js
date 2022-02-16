e15e87739543ce87e8a9c010688cb73cd517d89a
Alejandro <acuellarh@gmail.com>
2022-02-15 19:16:38 -0500

    Add bootstrap v5 and include a Title component

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+export const Title = () => {
+  return (
+    <>
+      <div className="row mt-4 pt-4 mb-3">
+        <div className="col">
+          <h1 className="text-center">Timers</h1>
+        </div>       
+      </div>
+    </>
+  )
+}
